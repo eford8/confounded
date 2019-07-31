Fixed bug that occurred when scaling small batches.

code/metrics/mmd.py
@@ -37,10 +37,10 @@ def mmd_multi_batch(batches):
             mmds.append(mmd(batches[i], batches[j]))
     return np.array(mmds).mean()
 
-def calculate_mmd(df, batch_col, log_adjust=False):
+def calculate_mmd(df, batch_col):
     batches = split_into_batches(df, batch_col)
-    if log_adjust:
-        batches = tuple([log_scale(batch) for batch in batches])
+#    if log_adjust:
+#        batches = tuple([log_scale(batch) for batch in batches])
     return mmd_multi_batch(batches)
 
 # Open files
@@ -70,7 +70,7 @@ for inpath in args.input_files:
             batch_col = "plate"
         else:
             batch_col = "Batch"
-        value = calculate_mmd(df, batch_col, log_adjust=("TCGA" in dataset.upper()))
+        value = calculate_mmd(df, batch_col)
         # Store metrics in logger
         logger.log(adjuster, dataset, value)
 

code/metrics/mse.py
@@ -9,12 +9,12 @@ parser.add_argument("-o", "--output-path", help="Path to output file", required=
 args = parser.parse_args()
 
 
-def calculate_mse(df1, df2, log_adjust=False):
+def calculate_mse(df1, df2):
     _, genes1 = split_discrete_continuous(df1)
     _, genes2 = split_discrete_continuous(df2)
-    if log_adjust:
-        genes1 = log_scale(genes1)
-        genes2 = log_scale(genes2)
+#    if log_adjust:
+#        genes1 = log_scale(genes1)
+#        genes2 = log_scale(genes2)
     squared_error = (np.array(genes1) - np.array(genes2))**2
     return squared_error.mean()
 
@@ -37,7 +37,7 @@ for inpath in args.input_files:
         else:
             adjuster = no_ext.replace(dataset, "").lstrip("_")
         print("Calculating MSE for the {} x {} dataset".format(dataset, adjuster))
-        value = calculate_mse(unadj, df, log_adjust=("TCGA" in dataset.upper()))
+        value = calculate_mse(unadj, df)
         # Store metrics in logger
         logger.log(adjuster, dataset, value)
 