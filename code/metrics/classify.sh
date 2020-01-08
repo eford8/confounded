50aac8d0f5f542b9fce30430f112de1494971a9a
srp33 <stephen_piccolo@byu.edu>
2020-01-08 14:53:49 -0700

    Initial draft of generating simulated expression data, need to improve.

--- code/metrics/classify.sh
+++ code/metrics/classify.sh
@@ -9,14 +9,17 @@ true_out_path="/output/metrics/true_classification.csv"
 
 rm -f ${batch_out_path} ${true_out_path}
 
+python classify.py -i /data/simulated_expression -o ${batch_out_path} -c Batch
+python classify.py -i /data/simulated_expression -o ${true_out_path} -c Class
+
 #python classify.py -i /data/mnist -o ${batch_out_path} -c Batch
 #python classify.py -i /data/mnist -o ${true_out_path} -c Digit
 
 # It doesn't make sense to measure true-class accuracy for bladderbatch because it is confounded with batch
 #python classify.py -i /data/bladderbatch -o ${batch_out_path} -c batch
 
-python classify.py -i /data/gse37199 -o ${batch_out_path} -c plate
-python classify.py -i /data/gse37199 -o ${true_out_path} -c Stage
+#python classify.py -i /data/gse37199 -o ${batch_out_path} -c plate
+#python classify.py -i /data/gse37199 -o ${true_out_path} -c Stage
 
 #for dataset in tcga tcga_medium tcga_small
 #do
--- code/prepdata/simulate_expression.py
+++ code/prepdata/simulate_expression.py
@@ -1,43 +1,50 @@
+import numpy as np
+import random
 from sklearn import preprocessing
-from sklearn.datasets import make_classification
+#from sklearn.datasets import make_classification
+from sklearn.datasets import make_checkerboard
 import sys
 
 out_file_path = sys.argv[1]
 
-n_samples = 100 # Must be an even number
-n_random_features = 800
-n_informative_features = 25
-n_redundant_features = 175
-#n_redundant_features = 175
-#n_random_features = 19800
-#n_informative_features = 25
-#n_redundant_features = 175
-class_sep = 1.0
-random_state = 0
+n_samples = 400
+n_features = 10000
+n_clusters = 4
+n_samples_per_cluster =  int(n_samples / n_clusters)
 
-n_features = n_random_features + n_informative_features + n_redundant_features
+X, rows, columns = make_checkerboard(
+    shape=(n_samples, n_features), n_clusters=(n_clusters, n_clusters), noise=1,
+    shuffle=False, random_state=0, minval=-3, maxval=3)
 
-#sklearn.datasets.make_classification(n_samples=100, n_features=20, n_informative=2, n_redundant=2, n_repeated=0, n_classes=2, n_clusters_per_class=2, weights=None, flip_y=0.01, class_sep=1.0, hypercube=True, shift=0.0, scale=1.0, shuffle=True, random_state=None)[source]¶
-X, y = make_classification(n_samples, n_features, n_informative_features, n_redundant_features, n_clusters_per_class=1, random_state=random_state, shuffle=False, flip_y=0.0, class_sep = class_sep)
+#X = preprocessing.scale(X)
 
-X = preprocessing.scale(X)
-
-labels = [i % 2 for i in range(n_samples)]
+labels = [0] * n_samples_per_cluster + [1] * n_samples_per_cluster + [0] * n_samples_per_cluster + [1] * n_samples_per_cluster
+batches = [0] * n_samples_per_cluster + [0] * n_samples_per_cluster + [1] * n_samples_per_cluster + [1] * n_samples_per_cluster
 
 ## Used this code to verify that we can differentiate the classes
-#from sklearn.svm import LinearSVC
-#clf = LinearSVC(C=1.0)
-#from sklearn.model_selection import cross_val_score
-#scores = cross_val_score(clf, X, y, cv=5)
-#print(scores)
-#sys.exit(0)
+from sklearn.svm import SVC
+clf = SVC(gamma="scale", random_state=0)
+from sklearn.model_selection import cross_val_score
+print("Predicting true labels:")
+print(np.mean(cross_val_score(clf, X, labels, cv=5)))
+print("Predicting randomized labels:")
+random.seed(0)
+random_labels = list(labels)
+random.shuffle(random_labels)
+print(np.mean(cross_val_score(clf, X, random_labels, cv=5)))
+print("Predicting batches:")
+print(np.mean(cross_val_score(clf, X, batches, cv=5)))
+print("Predicting randomized batches:")
+random_batches = list(batches)
+random.shuffle(random_batches)
+print(np.mean(cross_val_score(clf, X, random_batches, cv=5)))
 
 with open(out_file_path, 'w') as out_file:
-    out_file.write("\t".join(["ID", "Class", "Batch"] + ["Gene%i" % i for i in range(1, n_features + 1)]) + "\n")
+    out_file.write(",".join(["ID", "Class", "Batch"] + ["Gene%i" % i for i in range(1, n_features + 1)]) + "\n")
 
-    i = 1
+    i = 0
     for row in X:
-        batch = y[i-1]
-        label = "Label{}".format(labels[i-1])
-        out_file.write("\t".join(["Sample%i" % i, label, str(batch)] + [str(x) for x in row]) + "\n")
+        batch = str(batches[i])
+        label = str(labels[i])
+        out_file.write(",".join(["Sample{}".format(i + 1), label, batch] + [str(x) for x in row]) + "\n")
         i += 1
